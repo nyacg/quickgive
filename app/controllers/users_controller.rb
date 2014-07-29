e230354cdafec2eed9ff7ea77931eeac4d72fd41
Harry Rickards <hrickards@gmail.com>
2014-07-29 15:56:40 +0100

    Users controller commenting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
+  # Sign up as a new campaigner (more information required)
   def new_campaigner
   end
 
+  # Sign up as a new donor (less information required)
   def new_donor
+  end
+
+  def create
   end
 end
