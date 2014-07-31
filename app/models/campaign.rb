e9eaa331bbdcaa6335b6ba7a96a9d087b8fdc57b
Harry Rickards <hrickards@gmail.com>
2014-07-31 10:12:58 +0000

    Dashboard integration

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,6 +2,7 @@ class Campaign
   include MongoMapper::Document
 
   belongs_to :user
+  many :payments
   key :title,       String, required: true
   key :slug,        String, required: true, indexed: true
   key :description, String
@@ -12,6 +13,10 @@ class Campaign
 
   def self.find(slug)
     first slug: slug
+  end
+
+  def total_raised
+    payments.inject(0) { |sum, p| sum + p.amount.to_f }
   end
 
   many :payments
