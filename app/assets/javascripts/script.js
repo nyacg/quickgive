54d3887a286c0c0ee3f08045646863de342b50a9
nyacg <robert.j.h.chandler@googlemail.com>
2014-07-31 20:26:49 +0100

    Starting the evening

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,13 +1,6 @@
 $(window).load(function (){
 	
-	var viewportHeight = $(window).height();
-	var viewportWidth = $(window).width();
-	var $bg = $('.background-image');
-	var bgHeight = $bg.height();
-	var bgWidth = $bg.width();
-	var $heightElements = $(".above-container, .timeline-container");
-	//console.log(bgHeight + " " + viewportHeight);
-
+	//some test data
 	var campaignData = {
 		campaignerName: "Robert Chandler",
 		campaignerTwitter: "nyacg",
@@ -19,24 +12,33 @@ $(window).load(function (){
 		donors: [{name: "Robert", date: "01/02/14", amount: 50, via: "Twitter"}, {name: "Harry", date: "07/02/14", amount: 50, via: "Facebook"}, {name: "Vesko", date: "07/02/14", amount: 50, via: "Facebook"}]
 	};
 
+	//get some commonly used values as variables
+	var viewportHeight = $(window).height();
+	var viewportWidth = $(window).width();
+	var $bg = $('.background-image');
+	var bgHeight = $bg.height();
+	var bgWidth = $bg.width();
+	var $heightElements = $(".above-container, .timeline-container");
+	//console.log(bgHeight + " " + viewportHeight);
+
 	$('.timeline-badge').hide();
 	//console.log(campaignData);
 	
 	if(bgHeight >= viewportHeight * 1.1){
 		$heightElements.height(viewportHeight * 1.1);
 	} else {
-		$heightElements.height(bgHeight - 50);
+		$heightElements.height(bgHeight - 100);
 		//$('head').append("<style type='text/css'>.timeline:before{height: " + bgHeight-50 + "px !important;}</style>");
 	}
 
 	var timelineHeight = viewportHeight;
 
 	if(viewportWidth <= 991){
-		console.log(viewportWidth + " " + viewportHeight);
+		//console.log(viewportWidth + " " + viewportHeight);
 		$('.timeline-container').height(viewportHeight * 1.5);
-		console.log($('.timeline-container').height() + $('.text-container').height());
-		$('above-container').height(20);
+		$('.above-container').css("height", /*$('.timeline-container').height() + $('.text-container').height() + "px"*/ "100%");
 		timelineHeight = viewportHeight * 1.5;
+		$('.text-container').removeClass("big");
 	}
 
 	drawTimeline(timelineHeight, campaignData);
@@ -45,7 +47,7 @@ $(window).load(function (){
 		$bg.css('position', 'absolute');
 	}*/
 
-
+	//show the tooltip/labels things when you hover over a circle
 	$('.timeline-badge').mouseover(function(){
 		var $panel = $(this).next('.timeline-panel');
 		$panel.show();
